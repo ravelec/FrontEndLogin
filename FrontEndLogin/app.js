45969ae4766f4d77868816f5accfe8509c334a5b
ravelec <ravelec@yahoo.com.br>
2015-09-10 19:42:58 -0300

    First Commit

---
+++ FrontEndLogin/app.js
@@ -0,0 +1,41 @@
+﻿// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['ionic', 'app.controllers', 'satellizer', 'ui.router'])
+
+    .config(function ($stateProvider, $urlRouterProvider, $authProvider) {
+
+        // Satellizer configuration that specifies which API
+        // route the JWT should be retrieved from
+        $authProvider.loginUrl = 'http://localhost/clinclin/public/api/auth/login';
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+      .state('loginState', {
+          url: '/login',
+          templateUrl: 'loginpage.html',
+          controller: 'loginController'
+      })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'signuppage.html',
+            controller: 'signUpController'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html',
+            controller: 'homeController'
+        });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+        
+});
---
+++ FrontEndLogin/js/controllers.js
@@ -0,0 +1,113 @@
+﻿angular.module('app.controllers', [])
+
+.controller('loginController', function ($scope, $state, $http, $auth, $rootScope) {
+
+    $scope.doLogin = function () {
+        
+        credentials = {
+            email: $scope.log.username,
+            password: $scope.log.pass
+        }
+        
+
+        // Use Satellizer's $auth service to login
+        $auth.login(credentials)
+            .then(function (response) {
+                $scope.log = {
+                    username: '',
+                    pass: ''
+                };
+
+                $http.get('http://localhost/clinclin/public/api/auth/user')
+                    .success(function (response) {
+                        // Stringify the returned data to prepare it
+                        // to go into local storage
+                        user = JSON.stringify(response.user);
+
+                        // Set the stringified user data into local storage
+                        localStorage.setItem('user', user);
+
+                        // The user's authenticated state gets flipped to
+                        // true so we can now show parts of the UI that rely
+                        // on the user being logged in
+                        $rootScope.authenticated = true;
+
+                        // Putting the user's data on $rootScope allows
+                        // us to access it anywhere across the app
+                        $rootScope.currentUser = response.user;
+
+
+                    })
+                    .error(function (response) {
+
+                    });
+
+                $state.go('home', {});
+
+            })
+            .catch(function (response) {
+                alert("LOGIN FAILED");
+                $scope.log = {
+                    username: '',
+                    pass: ''
+                };
+        });
+    }
+
+})
+.controller('signUpController', function ($scope, $state, $http) {
+    
+
+    $scope.doRegister = function () {
+
+      
+      $http.post('http://localhost/clinclin/public/api/auth/register', {
+            name: $scope.reg.name,
+            email: $scope.reg.email,
+            password: $scope.reg.pass,
+            password_confirmation: $scope.reg.cPass,
+            }).
+        success(function (response) {
+            alert("REGISTRATION SUCCESSFULLY DONE");
+            $scope.reg = {
+                name: '',
+                email: '',
+                pass: '',
+                cPass: '',
+            };
+            $state.go('loginState', {});
+            
+        }).
+        error(function (response) {
+            
+            $scope.reg = {
+                name: '',
+                email: '',
+                pass: '',
+                cPass: '',
+            };
+
+            alert('REGISTRATION FAILED');
+        });
+
+    }
+})
+.controller('homeController', function ($scope, $state, $http, $rootScope, $auth) {
+
+    $scope.logout = function () {
+
+        $auth.logout().then(function () {
+
+            // Remove the authenticated user from local storage
+            localStorage.removeItem('user');
+
+            // Flip authenticated to false so that we no longer
+            // show UI elements dependant on the user being logged in
+            $rootScope.authenticated = false;
+
+            // Remove the current user info from rootscope
+            $rootScope.currentUser = null;
+        });
+    }
+
+});
